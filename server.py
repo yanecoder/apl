d7632e5c79ca155ff2f202c03727dbf9e0be121e
Dmitry <yanecoder@Dmitry--MacBook-Air.local>
2025-08-23 12:05:39 +0300

    update

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask, request
-from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-clients = {}  # id -> sid
+clients = {}
 
 @socketio.on('register')
 def handle_register(data):
